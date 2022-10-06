6502dd1b2167b9fa0edef01ac826262f67af3011
zzNe0zz <zzhoangzz1995@gmail.com>
2022-10-06 14:35:31 +0700

    hoang

--- src/compunent/Home.jsx
+++ src/compunent/Home.jsx
@@ -6,54 +6,45 @@ import "./Home.css"
 import { json } from 'react-router-dom';
 import {useEffect} from "react"
 function Home() {
-
-const data = [
-        { name: 'John Brown', description: 'New York No. 1 Lake Park',  tags: 'Kém',},
-        {  name: 'Jim Green', description: 'London No. 1 Lake Park', tags: 'Cần làm', },
-        { name: 'Joe Black',description: 'Sidney No. 1 Lake Park',tags: 'Làm gấp',},
-      ];
  const selectOTP = ["Kém","Cần làm","Làm gấp"]
- let[ bigData , setBigData] = useState(data)
- let local = JSON.stringify(bigData)
+ let[ bigData , setBigData] = useState([])
  let clone = ""
 let [idex,setIdex] = useState("")
-    // localStorage.getItem("toDo")?clone=JSON.parse(localStorage.getItem("toDo")):localStorage.setItem("toDo",`${local}`)
-
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-  // const showModal = () => {
-  //   setIsModalOpen(true);
-  // };
-
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [show,setShow] = useState(false)
+function getData (){
+  let local = localStorage.getItem("toDO")
+  local ? setBigData(JSON.parse(local)) : localStorage.setItem("toDO","[]")
+  
+}
   const handleOk = () => {
     setIsModalOpen(false);
     
   };
-
   const handleCancel = () => {
     setIsModalOpen(false);
   };
  function showModal1(){
    setIsModalOpen(true);
    let bntFix = document.querySelector(".bnt-ok").setAttribute("style"," display: none")
-  //  bntFix.setAttribute("style"," display: none")
-   console.log(bntFix);
  }
  function getIndex (x){
   setIsModalOpen(true);
   setIdex(x)
   let bntFix = document.querySelector(".bnt-ok")
   bntFix.setAttribute("style"," display: inline-block")
-  // let bntA = document.querySelector("#add")
-  // console.log(bntA);
  }
 function ADD (){
+  
     let name = document.querySelector("#name").value
     let description = document.querySelector("#description").value
     let tags = document.querySelector("#selec").value
-   
-    setBigData([...bigData,{name,description,tags}])
+    clone = [...bigData]
+    clone.push({name,description,tags})
+    setBigData(clone)
     setIsModalOpen(false);
+    localStorage.setItem("toDO",JSON.stringify(clone))
+    
 }
 function Fixdata() {
   let name = document.querySelector("#name").value
@@ -63,12 +54,15 @@ function Fixdata() {
  
   setBigData([...bigData])
   setIsModalOpen(false);
+  localStorage.setItem("toDO",JSON.stringify(bigData))
+ 
 }
 function Dele(x){
   console.log(x);
   bigData.splice(x,1)
-  
   setBigData([...bigData])
+  localStorage.setItem("toDO",JSON.stringify(bigData))
+
 }
 function Success (x){
   let text = document.querySelector(`.index${x}`)
@@ -122,7 +116,7 @@ function Success (x){
       ];
       
       useEffect(() => {
-        
+        getData() 
  
       }, [])
       
@@ -135,7 +129,7 @@ function Success (x){
                           </Button>
                 </div>
                  <div>
-                         <Table columns={columns} dataSource={bigData} />
+                         <Table columns={columns} dataSource={bigData}  pagination={{ defaultCurrent: 1,total:bigData.length }}/>
                  </div>
                  <div>
 
@@ -143,7 +137,7 @@ function Success (x){
                             <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={[
                                 <Button key="back" onClick={handleCancel}> Cancel </Button>,
                                 <Button key="submit" type="primary"  className='bnt-ok' onClick={Fixdata}> Fix</Button>,
-                                <Button key="submit" type="primary"  onClick={ADD} >Add</Button>,]} >
+                                <Button key="submit" type="primary"  onClick={ ADD} >Add</Button>,]} >
                                     <div className='inp'>
                                             <input type="text"  placeholder='Việc cần làm' id='name' />
                                             <input type="text"  placeholder='Mô tả công việc ' id='description'/> 
